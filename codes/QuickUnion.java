6a3450feaef8a1c9bfb6d1551d6b8576c537cfc6
SNN <snndmr@users.noreply.github.com>
2020-05-02 23:34:55 +0300

    Create QuickUnion.java

---
+++ codes/QuickUnion.java
@@ -0,0 +1,49 @@
+package codes;
+
+public class QuickUnion {
+	private int[] id;
+
+	public QuickUnion(int N) {
+		id = new int[N];
+
+		for (int i = 0; i < id.length; i++) {
+			id[i] = i;
+		}
+	}
+
+	private int root(int i) {
+		while (i != id[i]) {
+			i = id[i];
+		}
+		return i;
+	}
+
+	boolean isConnected(int p, int q) {
+		return root(p) == root(q);
+	}
+
+	public void union(int p, int q) {
+		id[root(p)] = root(q);
+	}
+
+	public static void main(String[] args) {
+		QuickUnion quickUnion = new QuickUnion(10);
+
+		System.out.println(quickUnion.isConnected(2, 5));
+
+		quickUnion.union(2, 5);
+		quickUnion.union(5, 4);
+		quickUnion.union(4, 3);
+
+		quickUnion.union(7, 8);
+		quickUnion.union(8, 9);
+		quickUnion.union(9, 1);
+
+		quickUnion.union(1, 5);
+
+		System.out.println(quickUnion.isConnected(2, 9));
+		System.out.println(quickUnion.isConnected(6, 9));
+
+		System.out.println(quickUnion.root(1));
+	}
+}
